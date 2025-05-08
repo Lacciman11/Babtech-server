82d2535d8ee5ce79ae6338f8361e7188e5ac59a4
Lacciman11 <nwabekeyiprecious@gmail.com>
2025-05-08 10:39:41 +0100

    still adjusted cors

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,12 +2,15 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: (origin, callback) => {
-    callback(null, true); // Accept all origins dynamically
+    if (!origin || /^https?:\/\/.+/.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
-  methods: ['POST', 'GET', 'PATCH', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
-
 
 module.exports = cors(corsOptions);
